6974076a3917dcbeb08f2fe2d67f847d1f0a7642
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-02-09 00:01:42 -0400

    Create Tarea 2.cs

---
+++ Tarea 2/Tarea 2.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Solicitar un número al usuario
+        Console.Write("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        // Verificar si el número es par o impar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número " + numero + " es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número " + numero + " es impar.");
+        }
